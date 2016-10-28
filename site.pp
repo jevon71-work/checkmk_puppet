0112992da098b4cbb4f6ca1685980731faacf68e
muhammad madasir <muhammad.madasir@dhigroupinc.com>
2016-10-28 14:32:59 +0100

    check mk refactor working

--- site.pp
+++ site.pp
@@ -1,21 +1,5 @@
 
 node '' {
-
-  notify {"hello checkmk Server!":}
+`     notify {"hello checkmk Server!":}
       include check_mk
-
 }
-
-# execute 'apt-get update'
-
-exec { 'apt-update':                    # exec resource named 'apt-update'
-  command => '/usr/bin/apt-get update'  # command this resource will run
-}
-
-
-
-Exec{ 'apt-get -f install':
-  command => '/usr/bin/apt-get -f install'
-
-}
-
