19b7b2a86586feb8d74b4e637c34c061b308be27
muhammad madasir <muhammad.madasir@dhigroupinc.com>
2016-11-01 12:06:10 +0000

    updated code

--- init.pp
+++ init.pp
@@ -1,3 +1,4 @@
+class check_mk {
 
 $my_deps = ['apache2-bin', 'apache2-data', 'apache2-mpm-prefork',
      'apache2-utils','curl', 'debugedit', 'dialog', 'fontconfig',
@@ -23,7 +24,7 @@ $my_deps = ['apache2-bin', 'apache2-data', 'apache2-mpm-prefork',
      'libsnmp-perl', 'libsnmp30', 'libtalloc2', 'libtdb1', 'libtevent0',
      'libthai-data', 'libthai0', 'libtiff5', 'libvpx1', 'libwbclient0',
      'libwebp5', 'libwebpmux1', 'libxaw7', 'libxcb-render0', 'libxcb-shm0',
-     'libxft2', 'libxmu6', 'libxpm4', 'libxrender1', 'libxt6', 'php-pear',
+     'libxft3', 'libxmu6', 'libxpm4', 'libxrender1', 'libxt6', 'php-pear',
      'php5-cgi', 'php5-cli', 'php5-common', 'php5-gd', 'php5-json', 'php5-mcrypt',
      'php5-readline', 'php5-sqlite', 'poppler-utils', 'pyro', 'python-crypto',
      'python-imaging', 'python-ldap', 'python-ldb', 'python-netsnmp', 'python-openssl',
@@ -31,13 +32,14 @@ $my_deps = ['apache2-bin', 'apache2-data', 'apache2-mpm-prefork',
      'python-samba', 'python-support', 'python-talloc', 'python-tdb', 'rpm', 'rpm-common',
      'rpm2cpio', 'samba-common', 'samba-common-bin', 'samba-libs', 'smbclient',
      'snmp','ssl-cert', 'traceroute', 'unzip', 'update-inetd', 'x11-common',
+     'libreadline5', 'xinetd', 'binutils',
       ]
 
   package {
     $my_deps: ensure => present,
   }
 
-    wget::fetch { "get checkmk blob":
+    wget::fetch { "get checkmk":
       source      => 'https://mathias-kettner.de/support/1.2.8p11/check-mk-raw-1.2.8p11_0.trusty_amd64.deb',
       destination => '/tmp/',
       timeout     => 0,
