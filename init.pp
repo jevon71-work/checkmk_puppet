0b1b632d422be218ad2bc41ad60687cf438c30c7
root <root@ip-172-31-16-36.eu-central-1.compute.internal>
2016-11-25 13:42:28 +0000

    confirmed working on ubuntu 14.04

--- init.pp
+++ init.pp
@@ -24,7 +24,7 @@ $my_deps = ['apache2-bin', 'apache2-data', 'apache2-mpm-prefork',
      'libsnmp-perl', 'libsnmp30', 'libtalloc2', 'libtdb1', 'libtevent0',
      'libthai-data', 'libthai0', 'libtiff5', 'libvpx1', 'libwbclient0',
      'libwebp5', 'libwebpmux1', 'libxaw7', 'libxcb-render0', 'libxcb-shm0',
-     'libxft3', 'libxmu6', 'libxpm4', 'libxrender1', 'libxt6', 'php-pear',
+     'libxft2', 'libxmu6', 'libxpm4', 'libxrender1', 'libxt6', 'php-pear',
      'php5-cgi', 'php5-cli', 'php5-common', 'php5-gd', 'php5-json', 'php5-mcrypt',
      'php5-readline', 'php5-sqlite', 'poppler-utils', 'pyro', 'python-crypto',
      'python-imaging', 'python-ldap', 'python-ldb', 'python-netsnmp', 'python-openssl',
@@ -32,20 +32,18 @@ $my_deps = ['apache2-bin', 'apache2-data', 'apache2-mpm-prefork',
      'python-samba', 'python-support', 'python-talloc', 'python-tdb', 'rpm', 'rpm-common',
      'rpm2cpio', 'samba-common', 'samba-common-bin', 'samba-libs', 'smbclient',
      'snmp','ssl-cert', 'traceroute', 'unzip', 'update-inetd', 'x11-common',
-     'libreadline5', 'xinetd', 'binutils',
+     'libreadline5', 'xinetd', 'binutils', 
       ]
 
   package {
     $my_deps: ensure => present,
-  }
-
-    wget::fetch { "get checkmk":
+  }->
+    wget::fetch { "get checkmk blob":
       source      => 'https://mathias-kettner.de/support/1.2.8p11/check-mk-raw-1.2.8p11_0.trusty_amd64.deb',
       destination => '/tmp/',
       timeout     => 0,
       verbose     => true,
-    }
-
+    }->
   package { 'check_mk_raw_1.2.8p11_0.trusty_amd64.deb':
     provider => 'dpkg',
     ensure   => present,
